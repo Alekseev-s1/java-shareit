c42ec371272aee12c9c3970cea30ddbaccf4fbc4
sergey <alles1028@gmail.com>
2022-08-10 13:25:02 +0300

    Добавлены предварительные model, dto и mapper классы

---
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit.booking;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+public class Booking {
+    private long id;
+    private LocalDate start;
+    private LocalDate end;
+    private 
+}
